2d223b8f17387f4ecd56d8a65b732197b250a44b
noredcat <anastasiaredcat@gmail.com>
2022-10-31 22:26:46 +0700

    delete properties

--- src/test/java/tests/MenuItem.java
+++
@@ -1,5 +0,0 @@
-package tests;
-
-public enum MenuItem {
-    BOOKS, COMPUTERS, ELECTRONICS
-}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.DriverConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -20,34 +18,29 @@ public class TestBase {
     static void configure() throws MalformedURLException {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
+
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://www.dns-shop.ru/";
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        System.setProperty("properties", "remote");
-        DriverConfig config = ConfigFactory.create(DriverConfig.class, System.getProperties());
+
+
 
         if (System.getProperty("remote_url") != null) {
-            capabilities.setCapability("browserName", config.getBrowser());
-            Configuration.browserSize = config.getBrowserSize();
-            Configuration.baseUrl = config.getBaseUrl();
-            Configuration.browserVersion = config.getBrowserVersion();
-        }
-        else {
-            System.setProperty("properties", "local");
-            DriverConfig configLoc = ConfigFactory.create(DriverConfig.class, System.getProperties());
-            capabilities.setCapability("browserName", configLoc.getBrowser());
-            Configuration.browserSize = configLoc.getBrowserSize();
-            Configuration.baseUrl = configLoc.getBaseUrl();
-            Configuration.browserVersion = configLoc.getBrowserVersion();
-        }
 
+            Configuration.browser = System.getProperty("browser_name");
+            Configuration.browserVersion = System.getProperty("browser_version");
+            Configuration.browserSize = System.getProperty("browser_size");
+            Configuration.remote = System.getProperty("remote_url");
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+        } else {
+            capabilities.setCapability("browserName", "chrome");
+            capabilities.setCapability("browserVersion", "100.0");
+            Configuration.browserSize = "1800x1200";
+
+
+        }
     }
-
-
-
     @AfterEach
     void addAttachments() {
         Attach.screenshotsAs("Last screenshot");
